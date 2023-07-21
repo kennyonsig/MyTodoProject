7d7ab77b7eeac5d1a0ef873667c12b50b773864c
kennyonsig <kennyonsig@gmail.com>
2023-07-21 15:08:12 +0300

    add input, task, todo-button, todo-list components

---
+++ src/app/components/task-button/task-button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-button.component.html',
+  styleUrls: ['./task-button.component.scss']
+})
+export class TaskButtonComponent {
+
+}
