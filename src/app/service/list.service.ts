9c2da5709829bd25684774bceffcf273a43725f3
kennyonsig <kennyonsig@gmail.com>
2023-08-05 14:43:58 +0300

    test new branch

--- src/app/service/list.service.ts
+++ src/app/service/list.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, take } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { IList } from '../model/IList';
 
 @Injectable({
@@ -12,70 +12,28 @@ export class ListService {
 
   private lists$ = new BehaviorSubject<IList[]>([]);
 
-  getLists(): Observable<IList[]> {
-    return this.lists$;
+  getLists() {
+
   }
 
   addNewList() {
-    this.lists$
-      .pipe(
-        take(1)
-      )
-      .subscribe((currentList: IList[]) => {
-        const currentListLength = currentList.length;
 
-        const newList: IList = {
-          listDate: this.listDate,
-          listName: this.listName,
-          listNumber: currentListLength + 1,
-          tasks: [],
-        };
-
-        const updateList: IList[] = [...currentList, newList];
-        this.lists$.next(updateList);
-
-      });
   }
 
   deleteAllList() {
-    this.lists$.next([]);
+
   }
 
   updateList(lists: IList[]) {
-    this.lists$.next(lists);
+    ;
   }
 
   duplicateList(selectedDupListNumber: number) {
-    this.lists$
-      .pipe(take(1))
-      .subscribe((currentList: IList[]) => {
-        const currentListLength = currentList.length;
 
-        const fillTasks = [...currentList[selectedDupListNumber - 1].tasks];
-
-        const duplicateList: IList = {
-          listDate: this.listDate,
-          listName: this.listName,
-          listNumber: currentListLength + 1,
-          tasks: fillTasks,
-        };
-
-        const updateList: IList[] = [...currentList, duplicateList];
-        this.lists$.next(updateList);
-      });
   }
 
   deleteList(selectedDelListNumber: number) {
-    this.lists$
-      .pipe(take(1))
-      .subscribe((currentList: IList[]) => {
-        const updateList: IList[] = currentList
-          .filter((list: IList) => list.listNumber !== selectedDelListNumber);
-        updateList
-          .forEach((list: IList, index: number) => list.listNumber = index + 1);
-        this.lists$
-          .next(updateList);
-      });
+
   }
 
 
